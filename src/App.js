3d42f3173b2582b395088c3a10ca844ccfd992a5
Taejun Kim <tjkimin2021@gmail.com>
2023-01-26 11:17:00 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
+import { Container, List, Paper, Grid, Button, AppBar, Toolbar, Typography } from '@material-ui/core';
+import React from 'react';
+import AddTodo from './AddTodo';
 import './App.css';
+import Todo from './Todo';
+import { call, signout } from "./service/ApiService";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    call("/todo", "GET", null).then((response) => {
+      this.setState({ items: response.data, loading: false });
+      }
+    );
+  }
+
+  add = (item) => {
+    call("/todo", "POST", item).then((response) =>
+      this.setState({ items: response.data, loading: false })
+    );
+  };
+
+  delete = (item) => {
+    call("/todo", "DELETE", item).then((response) => {
+      this.setState({ items: response.data, loading: false })
+    });
+  };
+
+  update = (item) => {
+    call("/todo", "PUT", item).then((response) => {
+      this.setState({ items: response.data, loading: false })
+    });
+  };
+  
+  render() {
+    let todoItems = this.state.items.length > 0 && (
+      <Paper style={{ margin: 16 }}>
+        <List>
+          {this.state.items.map((item, idx) => (
+            <Todo 
+              item={item} 
+              key={item.id} 
+              delete={this.delete} 
+              update={this.update}/>
+          ))}
+        </List>
+      </Paper>
+    );
+
+    let navigationBar = (
+      <AppBar position='static'>
+        <Toolbar>
+          <Grid justifyContent='space-between' container>
+            <Grid item>
+              <Typography variant='h6'>오늘의 할일</Typography>
+            </Grid>
+            <Grid>
+              <Button color='inherit' onClick={signout}>
+                로그아웃
+              </Button>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    );
+
+    let todoListPage = (
+      <div>
+        {navigationBar}
+        <Container maxWidth="md">
+          <AddTodo add={this.add}/>
+          <div className='TodoList'>{todoItems}</div>
+        </Container>
+      </div>
+    )
+
+    let loadingPage = <h1> 로딩중 </h1>;
+    let content = loadingPage;
+
+    if (!this.state.loading) {
+      content = todoListPage;
+    }
+    return (
+      <div className='App'>{content}</div>
+    )
+  }
 }
 
 export default App;
---
+++ src/service/ApiService.js
@@ -0,0 +1,62 @@
+import { API_BASE_URL } from "../api-config";
+const ACCESS_TOKEN = "ACCESS_TOKEN";
+
+export function call(api, method, request) {
+  let headers = new Headers({
+    "Content-Type": "application/json",
+  })
+
+  const accessToken = localStorage.getItem("ACCESS_TOKEN");
+  if (accessToken && accessToken !== null) {
+    headers.append("Authorization", "Bearer " + accessToken);
+  }
+
+  let options = {
+    headers: headers,
+    url: API_BASE_URL + api,
+    method: method,
+  };
+
+  if (request) {
+    options.body = JSON.stringify(request);
+  }
+
+  return fetch(options.url, options)
+    .then((response) => response.json().then((json) => {
+      if (!response.ok) {
+        return Promise.reject(json);
+      }
+      return json;
+    })
+  ).catch((error) => {
+    // 403을 받을 경우 로그인 페이지로 자동으로 리다이렉트 한다.
+    alert('아이디와 비밀번호를 확인하세요')
+    if (error) {
+      window.location.href = "/login";
+    }
+    return Promise.reject();
+  });
+}
+
+export function signin(userDTO) {
+  return call("/auth/signin", "POST", userDTO)
+  .then((response) => {
+    if (response.token) {
+      localStorage.setItem(ACCESS_TOKEN, response.token);
+      window.location.href = "/";
+    }
+  })
+}
+
+export function signout() {
+  localStorage.setItem(ACCESS_TOKEN, null);
+  window.location.href = "/login";
+}
+
+export function signup(userDTO) {
+  return call("/auth/signup", "POST", userDTO)
+  .then((response) => {
+    console.log(response);
+    window.location.href = "/login";
+  })
+}
